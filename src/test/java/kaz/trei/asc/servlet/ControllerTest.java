cd7eced7fd148888875dbdea3ab244a15aea9972
alhenk <alexandr.g.koryagin@gmail.com>
2014-04-13 22:55:45 +0600

    Mockito dependency added

--- src/test/java/kaz/trei/asc/servlet/ControllerTest.java
+++ src/test/java/kaz/trei/asc/servlet/ControllerTest.java
@@ -2,8 +2,16 @@ package kaz.trei.asc.servlet;
 
 import junit.framework.TestCase;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.mock;
+
 public class ControllerTest extends TestCase {
     public void testGetActionName() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
         String expected = "Hello, JUnit!";
         String hello = "Hello, JUnit!";
         assertEquals(hello, expected);
