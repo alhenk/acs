51135e121759a98cfa918267a0c69222c15cde60
alhenk <alexandr.g.koryagin@gmail.com>
2014-04-13 17:54:52 +0600

    ActionFactory minimalistic implementation

--- src/main/java/kaz/trei/asc/action/ActionFactory.java
+++ src/main/java/kaz/trei/asc/action/ActionFactory.java
@@ -1,7 +1,17 @@
 package kaz.trei.asc.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActionFactory {
+    private Map<String, Action> actions;
+
+    public ActionFactory(){
+        actions = new HashMap<String, Action>();
+        actions.put("GET/main", new ShowMain());
+    }
     public Action create(String actionName) {
+
         return null;
     }
 }
--- src/main/java/kaz/trei/asc/action/ActionResult.java
+++ src/main/java/kaz/trei/asc/action/ActionResult.java
@@ -1,7 +1,31 @@
 package kaz.trei.asc.action;
 
-/**
- * Created by Admin on 13.04.14.
- */
 public class ActionResult {
+    private ActionType method;
+    private String path;
+
+    public ActionResult() {
+        method = ActionType.REDIRECT;
+        path = "/";
+    }
+    public ActionResult(ActionType method, String path){
+        this.method = method;
+        this.path = path;
+    }
+
+    public ActionType getMethod() {
+        return method;
+    }
+
+    public void setMethod(ActionType method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
