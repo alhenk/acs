2cb6498b8b0f4aad9835ea7f93d795393f23f382
Alhen <alexandr.g.koryagin@gmail.com>
2014-04-14 10:00:13 +0600

    resources and log4j properties added

--- src/main/java/kaz/trei/asc/servlet/Controller.java
+++ src/main/java/kaz/trei/asc/servlet/Controller.java
@@ -3,6 +3,7 @@ package kaz.trei.asc.servlet;
 import kaz.trei.asc.action.Action;
 import kaz.trei.asc.action.ActionFactory;
 import kaz.trei.asc.action.ActionResult;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,12 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Controller extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(Controller.class);
     private ActionFactory actionFactory=new ActionFactory();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String actionName = getActionName(request);
         Action action = actionFactory.create(actionName);
+        LOGGER.debug("Action Name = "+actionName);
         ActionResult result = action.execute(request, response);
         switch (result.getMethod()){
             case FORWARD:
